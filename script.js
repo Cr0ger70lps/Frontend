631bf782f7fbbe846789b056f8c67f132a43253d
devlooper <yayatorree6@gmail.com>
2024-10-21 16:33:36 +0200

    add: i add the menu

--- script.js
+++ script.js
@@ -6,3 +6,35 @@ function dropdownMenu(){
         x.className = "nav-items"
     }
 }
+
+
+
+function navBorder() { 
+  let items = document.getElementsByClassName('item');
+  let menus = document.querySelectorAll('.menu-1, .menu-2, .menu-3'); // Use a selector for the specific menu classes
+
+  for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener('click', (function(index) {
+      return function() {
+        // Remove 'nav-border' from all items
+        for (let j = 0; j < items.length; j++) {
+          items[j].classList.remove("nav-border");
+          menus[j].style.display = "none";
+        }
+        // Add 'nav-border' to the clicked item
+        this.classList.add("nav-border");
+        menus[index].style.display = "block"; // Show the corresponding menu
+      };
+    })(i));
+  }
+}
+
+
+
+
+
+
+
+
+
+
